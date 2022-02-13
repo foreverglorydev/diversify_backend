3fab1327c6d251c381ef9ee838b41c0ad9d0762d
foreverglorydev <happyyada.glory@gmail.com>
2022-02-13 14:00:25 -0500

    file read and encrypt

--- server/index.js
+++ server/index.js
@@ -5,33 +5,45 @@ const keccak256 = require('keccak256');
 const cors = require("cors");
 const formidable = require("formidable");
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({origin: "http://localhost:3000"}));
 
-
-
-
-
-// let aaa = keccak256('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4').toString('hex');
-
-// console.log(aaa);
-// let bbb = keccak256("\x19Ethereum Signed Message:\n320x" + aaa).toString('hex');
-  // console.log(bbb);
-
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-let whitelistusers = ['0x4F56Bc08d5D75eD0252d6de31896a9F4f4d6DBc7', '0xDd9346E5b40892053eb706A046D142De1e4cCc26'];
 
-app.post("/api", (req, res) => {
-
-  let address = req.body.account;
-  console.log(address);
-  let signature = keccak256(address).toString('hex');
-  res.json({ message: signature });
+app.post("/api", (req, res) =>  {
   
+  let reqAddr = req.body.account;
+  let resAddr = 'none';   //default address of return value of this API
+
+  fs.readFile('./server/whitelists.txt', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+    // console.log(typeof data);
+    let userList = JSON.parse(content);
+    for(let i = 0; i < userList.length; i++)
+    {
+      if(userList[i].address == reqAddr)
+        resAddr = reqAddr;     //if match, replace content of return with matching address.
+    }
+
+  
+    console.log(userList.length);  
+    console.log(userList[1].user);
+    console.log(userList[1].address);  
+
+    //if matched real address is encrypted,   else static string "none" will be encrypted as a signature
+    //if none, no meanningful this hash.
+    let signature = keccak256(resAddr).toString('hex');   
+    res.json({ message: signature });
+
+  });
 });
 
 
